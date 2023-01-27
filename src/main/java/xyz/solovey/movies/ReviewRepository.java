ffd64120d8aac55d3228cf42e8ef22e273785b95
soloveyy <solovyov.dd@gmail.com>
2023-01-28 00:03:25 +0100

    Initial commit

---
+++ src/main/java/xyz/solovey/movies/ReviewRepository.java
@@ -0,0 +1,8 @@
+package xyz.solovey.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
